938137c1788635a2dc4fc33ef91171f04b8e5e46
Jim <516292026@qq.com>
2018-11-21 21:57:49 +0800

    router learn

---
+++ src/components/User.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>This is p in 111 User.vue</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"User",
+    }
+</script>
+
+<style>
+    
+</style>
