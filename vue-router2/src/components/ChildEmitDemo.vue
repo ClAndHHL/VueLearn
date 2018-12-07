45e1e28ec8ccef7500002b72462805d39c712575
Jim <516292026@qq.com>
2018-12-07 16:34:52 +0800

    自定义输入框

--- vue-router2/src/components/ChildEmitDemo.vue
+++ vue-router2/src/components/ChildEmitDemo.vue
@@ -1,7 +1,8 @@
 <template>
 <div id="blog-posts-events-demo">
   <div :style="{ fontSize: postFontSize + 'em' }">
-      <div>1111</div>
+    <div>1111</div>
+    <custome-input v-model="searchText"></custome-input>
     <blog-post
       v-for="post in posts"
       v-bind:key="post.id"
@@ -14,19 +15,22 @@
 
 <script>
 import BlogPost from "./BlogPost"
+import CustomeInput from "./CustomeInput"
+
 
 export default {
     name: 'ChildEmitDemo',
     data(){
         return {
             postFontSize:2,
+            searchText:"aaa",
             posts:[
                 {id:0,content:"0000"},
                 {id:1,content:"00001"}
                 ]
         }
     },
-    components:{BlogPost},
+    components:{BlogPost,CustomeInput},
 }
 </script>
 
