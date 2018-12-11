a33694aa438618572125badf1f9f02a646a37fc1
Jim <516292026@qq.com>
2018-12-11 21:22:43 +0800

    Container提交

--- element-learn/src/router/index.js
+++ element-learn/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import LayoutDemo from '@/views/layout/LayoutDemo'
+import ContainerDemo from '@/views/container/ContainerDemo'
+
 
 Vue.use(Router)
 
@@ -16,6 +18,11 @@ export default new Router({
       path: '/layoutdemo',
       name: 'LayoutDemo',
       component: LayoutDemo
+    },
+    {
+      path: '/containerdemo',
+      name: 'ContainerDemo',
+      component: ContainerDemo
     }
   ]
 })
