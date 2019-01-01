e3bb7147848a37f7a834d7989491f85327fd68ea
Jim <516292026@qq.com>
2019-01-01 21:29:14 +0800

    提交 vuex

---
+++ vuexlearn/vuexdemo/src/components/VuexPages.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <el-row>
+        {{count}}//{{this.$store.state.count}}////{{countAtlias}}
+    </el-row>
+</div>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+    data(){
+        return{
+
+        }
+    },
+    computed: {
+        count () {
+            return this.$store.state.count
+        },
+        ...mapState({
+            countAtlias:"count"
+        }),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
--- vuexlearn/vuexdemo/src/main.js
+++ vuexlearn/vuexdemo/src/main.js
@@ -4,14 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 10
   },
   mutations: {
     increment (state) {
