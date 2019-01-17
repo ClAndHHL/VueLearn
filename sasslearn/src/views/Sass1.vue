92bc9a558492291d9f4593d741b0467933afedcc
Jim <516292026@qq.com>
2019-01-17 15:04:22 +0800

    提交demo1

---
+++ sasslearn/src/views/Sass1.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>this is header
+            <p>this is p in header</p>
+        </h3>    
+        <P>this is p under header</P>
+        <P>this is p under header2</P>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    h3 + p { 
+        font-size: 2em;
+        color:green;
+    }
+    // p {
+    //     color: red;
+    // }
+</style>
+
