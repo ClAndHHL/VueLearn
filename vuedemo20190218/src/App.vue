553017ddc486bf201b54f0ce59a912d8097b7284
Jim <516292026@qq.com>
2019-02-19 16:43:05 +0800

    添加 element ui

--- vuedemo20190218/src/App.vue
+++ vuedemo20190218/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/elementdemo1">elementdemo1</router-link>|
+      <router-link to="/layoutdemo">layoutdemo</router-link>
     </div>
     <router-view/>
   </div>
--- vuedemo20190218/src/router.js
+++ vuedemo20190218/src/router.js
@@ -20,6 +20,22 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/elementdemo1',
+      name: 'elementdemo1',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/ElementDemo1.vue')
+    },
+    {
+      path: '/layoutdemo',
+      name: 'layoutdemo',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/LayoutDemo.vue')
+    },
   ]
 })
