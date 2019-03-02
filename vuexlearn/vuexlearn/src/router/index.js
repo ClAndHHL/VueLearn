99c9975bd1e890a2562dc8325838d56eff4eca62
Jim <516292026@qq.com>
2019-03-02 15:06:25 +0800

    添加 路由懒加载

--- vuexlearn/vuexlearn/src/router/index.js
+++ vuexlearn/vuexlearn/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import About from '@/views/About'
+
+
+const About = ()=>import("@/views/About");
+
 
 Vue.use(Router)
 
